03e49c8c2653d80a6196df580cf3d2c54d1033ff
James Kelleher <james.kelleher.3345@gmail.com>
2025-12-12 17:04:54 -0500

    update dashboard

--- scratchpad/sketches/devicecontrol.js
+++ scratchpad/sketches/devicecontrol.js
@@ -5,42 +5,108 @@ let portButton = null;
 
 let btn;
 
+let inputs = [];
+
+let currentValues = "";
+
 function setup() {
-    createCanvas(400, 300);
+  createCanvas(400, 300);
 
-    setupSerial();
+  setupSerial();
 
-    // btn = creaeButton("a");
-    // btn.html("tb");
+  let btnLabels = ["hfSpeed", "lfSpeed", "freqOne", "freqTwo", "ampOne", "ampTwo"];
+  for (let i = 0; i < 6; i++) {
+    let btnLabel = btnLabels[i];
+    let y = portButton.y + portButton.height + 20 + 30 * i;
 
-    
+    let input = createInput();
+    input.position(10, y);
+    input.size(70, input.height);
+    inputs.push(input);
+
+    let btn = createButton(btnLabel);
+    btn.position(input.x + input.width + 15, y + 4);
+
+    function submit() {
+      let val = input.value();
+      if (!isFinite(+val)) { console.log(`invalid data: ${val}`); return; }
+
+      serial.print(i);
+      serial.println(val);
+    }
+
+    input.changed(submit);
+    btn.mousePressed(submit);
+  }
 }
 
 function draw() {
-    background(220);
+  background(220);
+
+  if (currentValues) {
+    textSize(24);
+    stroke("black");
+    strokeWeight(3);
+    let split = currentValues.trim().split('\n');
+    let rows = currentValues.split('\n').slice(1, split.length);
+    for (let r = 0; r < rows.length; r++) {
+      let cols = rows[r].split(', ');
+      for (let c = 0; c < cols.length; c++) {
+        let elem = cols[c];
+        if (!elem) continue;
+        let val = elem.split(": ")[1];
+        let colorCode = val.slice(0, val.indexOf('m') + 1);
+        let color = ansi24ToRgb(colorCode);
+        let reading = val.slice(colorCode.length, val.indexOf('\x1B', colorCode.length));
+        fill(color.r, color.g, color.b);
+        text(reading, 200 + 60 * c, 80 + 40 * r);
+      }
+    }
+  }
 }
 
-function setupSerial() {
-    // check to see if serial is available
-    if (!navigator.serial) {
-        alert("WebSerial is not supported in this browser. Try Chrome or MS Edge.");
-    }
+function processInData() {
+  console.log(inData);
+  if (inData.startsWith("currentValues")) currentValues = inData;
+}
 
-    // if serial is available, add connect/disconnect listeners:
-    navigator.serial.addEventListener("connect", portConnect);
-    navigator.serial.addEventListener("disconnect", portDisconnect);
-    // check for any ports that are available:
-    serial.getPorts();
-    // if there's no port chosen, choose one:
-    makePortButton();
-    // serial.on("noport", makePortButton);
-    // open whatever port is available:
-    serial.on("portavailable", openPort);
-    // handle serial errors:
-    serial.on("requesterror", portError);
-    // handle any incoming serial data:
-    serial.on("data", serialEvent);
-    serial.on("close", makePortButton);
+function ansi24ToRgb(ansi) {
+  const match = ansi.match(/\x1b\[\d+;2;(\d+);(\d+);(\d+)m/);
+  if (!match) return null;
+  return {
+    r: Number(match[1]),
+    g: Number(match[2]),
+    b: Number(match[3]),
+  };
+}
+
+/*
+
+  SERIAL BOILERPLATE
+
+*/
+
+function setupSerial() {
+  // check to see if serial is available
+  if (!navigator.serial) {
+    alert("WebSerial is not supported in this browser. Try Chrome or MS Edge.");
+  }
+
+  // if serial is available, add connect/disconnect listeners:
+  navigator.serial.addEventListener("connect", portConnect);
+  navigator.serial.addEventListener("disconnect", portDisconnect);
+  // check for any ports that are available:
+  serial.getPorts();
+  // if there's no port chosen, choose one:
+  makePortButton();
+  // serial.on("noport", makePortButton);
+  // open whatever port is available:
+  serial.on("portavailable", openPort);
+  // handle serial errors:
+  serial.on("requesterror", portError);
+  // handle any incoming serial data:
+  serial.on("data", serialEvent);
+  serial.on("close", makePortButton);
 }
 
 
@@ -73,7 +139,7 @@ function openPort() {
   }
 
   // hide the port button once a port is chosen:
-//   if (portButton) portButton.hide();
+  //   if (portButton) portButton.hide();
   if (portButton) {
     portButton.html("close port");
     portButton.mousePressed(closePort);
@@ -88,11 +154,11 @@ function portError(err) {
 // read any incoming data as a string
 // (assumes a newline at the end of it):
 function serialEvent() {
-//   inData = Number(serial.read());
-//   console.log(inData);
-// read a byte from the serial port, convert it to a number:
+  //   inData = Number(serial.read());
+  //   console.log(inData);
+  // read a byte from the serial port, convert it to a number:
   inData = serial.readLine();
-  if (inData) console.log(inData);
+  if (inData) processInData();
 }
 
 // try to connect if a new serial port
@@ -111,6 +177,7 @@ function portDisconnect() {
 
 function closePort() {
   serial.close();
+  console.log("port closed");
   portButton.html("choose port");
   portButton.mousePressed(choosePort);
 }
